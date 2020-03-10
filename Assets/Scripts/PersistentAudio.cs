0d6612a4a7034712b8d6d9ef8942d03cfc0e61d1
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-03-10 21:10:55 +0100

    MenuFix

--- Assets/Scripts/PersistentAudio.cs
+++ Assets/Scripts/PersistentAudio.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PersistentAudio : MonoBehaviour
 {
      private AudioSource _audioSource;
+     private bool music = false;
      private void Awake()
      {
+         Debug.Log("Awake music : " + music);
          DontDestroyOnLoad(transform.gameObject);
          _audioSource = GetComponent<AudioSource>();
+         if (!music)
+         {
+             PlayMusic();
+             music = true;
+         }
+         
+         Debug.Log("AwakeLatter music : " + music);
      }
  
      public void PlayMusic()
      {
+         
          if (_audioSource.isPlaying) return;
          _audioSource.Play();
      }
