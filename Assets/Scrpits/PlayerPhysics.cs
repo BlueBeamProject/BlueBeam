e6cfbba3ed59c6e47daa8e52bed3ee3a6462d379
cepheus00 <grissamy23@gmail.com>
2020-03-10 17:28:08 +0100

    Implémentation de scripts
    Sur le niveau test (niveau 1):
    script PlayerPhysics & PlayerAnimations
    (PAS FINI)

---
+++ Assets/Scrpits/PlayerPhysics.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour
+{
+    private Transform position;
+    private int gravity;
+    public Vector2 velocity;
+    public bool grounded;
+    public int jumpForce;
+    public int moveSpeed;
+    public Collider2D myCollider;
+    public Animator myAnimator;
+    public LayerMask whatIsGround;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gravity = 5;
+        this.velocity = new Vector2(0,gravity);
+        this.position = GetComponent<Transform>();
+        this.jumpForce = 0;
+        this.moveSpeed = 2;
+        this.myCollider = GetComponent<Collider2D>();
+        this.myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        
+        velocity.y += gravity;
+        velocity.x += moveSpeed;
+        velocity.y -= jumpForce;
+        if (grounded)
+            velocity.y -= gravity;
+        else if (jumpForce > 0)
+        {
+            jumpForce--;
+        }
+
+        position.Translate(velocity.x,velocity.y,0);
+    }
+}
