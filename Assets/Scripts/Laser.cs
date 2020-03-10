45f6bb0e6d36f29c72a9bf2c1f7baeb6861b9262
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-03-11 00:45:55 +0100

    MenuUpdateNice

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -8,6 +8,7 @@ public class Laser : MonoBehaviour
     public float newPos;
     private float scrollSpeed = -30f;
     public GameObject logo;
+    public bool LogoShow = false;
 
     private Vector2 startPos;
     // Start is called before the first frame update
@@ -19,16 +20,18 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > 19.665 && Time.time < 25.207)
-        newPos += 0.25f;
-
-        if (Time.time > 22.0 && Time.time < 23.0)
+        if (Time.time > 19.365 && Time.time < 23.207)
         {
-            Debug.Log("logl");
-            logo.SetActive(true);
+            //19.625 et 25.207
+            newPos += 0.75f;
+            if (Time.time > 21.0 && !LogoShow)
+            {
+                logo.SetActive(true);
+            }
         }
         
         transform.position = startPos + Vector2.left * newPos;
+        
         
     }
 }
