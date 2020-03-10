b0ee3e13b329a7cc0d90ee3581848a888f9c586f
Mamadoudette <vicbut10@gmail.com>
2020-03-10 18:58:27 +0100

    je repare les betises de Samy

---
+++ Assets/Scrpits/ChangeOpacity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOpacity : MonoBehaviour
+{
+	public float alphaLevel = .5f;
+	public int fois = 0;
+	public int plus = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float time = Time.time;
+		if ((time > 0 && time < 0.170) || (time > 2.668 && time < 2.84) || (time > 4.001 && time < 4.17) || (time > 5.334 && time < 5.493)) alphaLevel += .10f;
+		if ((time > (2.668 + 5.333*fois) && time < (2.84 + 5.333*fois)) || (time > (4.001 + 5.333*fois) && time < (4.17 + 5.333*fois)) || (time > (5.334 + 5.333*fois) && time < (5.493 + 5.333*fois))) alphaLevel += .05f;
+		else if (alphaLevel > 0) alphaLevel -= .05f;
+		if (time > 5.493*(fois+1)) fois += 1;
+		GetComponent<SpriteRenderer>().color = new Color (1, 1, 1, alphaLevel);
+    }
+}
