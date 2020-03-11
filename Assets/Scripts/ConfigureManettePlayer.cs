5d0b43e82f8570cf3c9bbe700ed3724a6863f7e0
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-03-11 18:47:53 +0100

    fix

--- Assets/Scripts/ConfigureManettePlayer.cs
+++ Assets/Scripts/ConfigureManettePlayer.cs
@@ -11,7 +11,11 @@ public class ConfigureManettePlayer : MonoBehaviour
     public GameObject P2;
     public GameObject P3;
     public GameObject P4;
-    
+
+    private static int P1controllerKey;
+    private static int P2controllerKey;
+    private static int P3controllerKey;
+    private static int P4controllerKey;
     
     private bool configure = false;
     void Start()
@@ -27,8 +31,21 @@ public class ConfigureManettePlayer : MonoBehaviour
     }
 
 
-    void HowMuchPlayerNumber(int PlayerNumber)
+    public void HowMuchPlayerNumber(int PlayerNumber)
     {
-        
+        Debug.Log(PlayerNumber);
+
+        bool echap = false;
+        bool finish = false;
+        int i = 1;
+    
+        /*
+        while (i <= PlayerNumber && !echap && !finish)
+        {
+            Debug.Log("Player " + i + " : Please press Jump on your Keyboard or Controller (space or right button)");
+            
+        }
+        */
+
     }
 }
