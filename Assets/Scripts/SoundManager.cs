1ed7a0aa861d1c833e67794d2f3f0fa49aae2c90
Mamadoudette <vicbut10@gmail.com>
2020-03-11 16:59:26 +0100

    Ajout de bruitages

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip jumpSound;
+    public AudioClip landSound;
+
+    private AudioSource audioSrc;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+        audioSrc.clip = jumpSound;
+        audioSrc.Play();
+    }
+
+    public void Land()
+    {
+        audioSrc.clip = landSound;
+        audioSrc.Play();
+    }
+
+}
