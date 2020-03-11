9fbda542834a51dbb3e64afe1e175da4fc51a53e
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-03-11 01:55:04 +0100

    PusgLol

--- Assets/Scripts/ChangeMenu.cs
+++ Assets/Scripts/ChangeMenu.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ChangeMenu : MonoBehaviour
 {
-    public GameObject MainMenu;
+   /* public GameObject MainMenu;
     public GameObject CampagneMenu;
     public GameObject EndlessMenu;
     public GameObject BattleRoyaleMenu;
     public GameObject OptionsMenu;
+    public GameObject PlayerNumberMenu;
+    public GameObject PlayerConfigureMenu;*/
+
+   public GameObject menu;
+   public GameObject previousMenu;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -18,53 +24,96 @@ public class ChangeMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameObject.FindGameObjectWithTag("Menu").SetActive(false);
+            previousMenu.SetActive(true);
+        }
     }
     
-    public void ChangeMenuByName(string sceneName)
+    public void ChangeMenuByName()
     {
-        switch (sceneName)
-        {
-            case "MainMenu":
-                MainMenu.SetActive(true);
-                CampagneMenu.SetActive(false);
-                EndlessMenu.SetActive(false);
-                BattleRoyaleMenu.SetActive(false);
-                OptionsMenu.SetActive(false);
-                break;
-            case "CampagneMenu":
-                MainMenu.SetActive(false);
-                CampagneMenu.SetActive(true);
-                EndlessMenu.SetActive(false);
-                BattleRoyaleMenu.SetActive(false);
-                OptionsMenu.SetActive(false);
-                break;
-            case "EndlessMenu":
-                MainMenu.SetActive(false);
-                CampagneMenu.SetActive(false);
-                EndlessMenu.SetActive(true);
-                BattleRoyaleMenu.SetActive(false);
-                OptionsMenu.SetActive(false);
-                break;
-            case "BattleRoyaleMenu":
-                MainMenu.SetActive(false);
-                CampagneMenu.SetActive(false);
-                EndlessMenu.SetActive(false);
-                BattleRoyaleMenu.SetActive(true);
-                OptionsMenu.SetActive(false);
-                break;
-            case "OptionsMenu":
-                MainMenu.SetActive(false);
-                CampagneMenu.SetActive(false);
-                EndlessMenu.SetActive(false);
-                BattleRoyaleMenu.SetActive(false);
-                OptionsMenu.SetActive(true);
-                break;
-            default:
-                Debug.Log("tes con");
-                break;
-            
-                
-        }
+        
+        GameObject.FindGameObjectWithTag("Menu").SetActive(false);
+
+        menu.SetActive(true);
+        
+        
+
+
+        /* switch (sceneName)
+         {
+             case "MainMenu":
+                 MainMenu.SetActive(true);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(false);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "CampagneMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(true);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(false);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "EndlessMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(true);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(false);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "BattleRoyaleMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(true);
+                 OptionsMenu.SetActive(false);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "OptionsMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(true);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "PlayerNumberMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(false);
+                 PlayerNumberMenu.SetActive(true);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             case "PlayerConfigureMenu":
+                 MainMenu.SetActive(false);
+                 CampagneMenu.SetActive(false);
+                 EndlessMenu.SetActive(false);
+                 BattleRoyaleMenu.SetActive(false);
+                 OptionsMenu.SetActive(true);
+                 PlayerNumberMenu.SetActive(false);
+                 PlayerConfigureMenu.SetActive(false);
+                 break;
+             default:
+                 Debug.Log("tes con");
+                 break;
+             
+                 
+         }*/
+
+
+
     }
 }
---
+++ Assets/Scripts/ConfigureManettePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigureManettePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int PlayerNumber;
+    private bool configure = false;
+    void Start()
+    {
+       /* while (!configure && Input.GetKeyDown("echap"))
+        {
+            Debug.Log("bite");
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
