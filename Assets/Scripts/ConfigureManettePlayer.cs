091328f16340936132a1260b68538f3b73c4ce80
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-03-11 14:10:43 +0100

    MORNING

--- Assets/Scripts/ConfigureManettePlayer.cs
+++ Assets/Scripts/ConfigureManettePlayer.cs
@@ -6,9 +6,23 @@ public class ConfigureManettePlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     public int PlayerNumber;
+
+    public GameObject P1;
+    public GameObject P2;
+    public GameObject P3;
+    public GameObject P4;
+    
+    
     private bool configure = false;
     void Start()
     {
+
+        for (int i = 1; i <= PlayerNumber; i++)
+        {
+            
+        }
+        
+        
        /* while (!configure && Input.GetKeyDown("echap"))
         {
             Debug.Log("bite");
