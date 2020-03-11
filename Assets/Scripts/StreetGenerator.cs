6bb6d9374399f9492a20fc865bae41fdeebd09a9
cepheus00 <grissamy23@gmail.com>
2020-03-11 15:09:01 +0100

    Avancement dans le StreetGenerator

--- Assets/Scripts/StreetGenerator.cs
+++ Assets/Scripts/StreetGenerator.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class StreetGenerator : MonoBehaviour
 {
+    public Sprite[] frames;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject route = new GameObject();
+            route.name = "route" + i;
+            route.transform.SetParent(GetComponent<Transform>());
+            route.AddComponent<Transform>();
+            route.AddComponent<SpriteRenderer>();
+            SpriteRenderer sprite = route.GetComponent<SpriteRenderer>();
+            sprite.sprite = frames[i/frames.Length];
+            route.transform.position = new Vector3(i * frames[i/frames.Length].border.x, -4, 0);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
     }
 }
