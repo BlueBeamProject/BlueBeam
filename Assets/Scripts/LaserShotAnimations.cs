b2a60c7ed882295bc6fe95341066d7efb80c5407
Jojomonster0706 <bouboulover17@gmail.com>
2020-03-12 15:23:28 +0100

    Ennemi lvl 1

--- Assets/Scripts/LaserShotAnimations.cs
+++ Assets/Scripts/LaserShotAnimations.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class LaserShotAnimations : MonoBehaviour
 {
 
-    public float newPos;
-    private GameObject _ennemy = GameObject.FindGameObjectWithTag("Ennemy");
+    private float newPos;
+    public Sprite laserSprite;
+    private GameObject _ennemy;
     private Vector2 startPos;
-    private Vector2 _ennemyPosition; 
+    private Vector2 _ennemyPosition;
+    private bool activated;
+    private SpriteRenderer mySprite;
 
     void Start()
     {
+        newPos = 0;
+        activated = false;
+        _ennemy = GameObject.FindGameObjectWithTag("Ennemy");
         _ennemyPosition = new Vector2(_ennemy.transform.position.x, _ennemy.transform.position.y);
         startPos = _ennemyPosition + new Vector2(-3.29629f, 3.755829f);
+        mySprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        newPos += 0.75f;
-        transform.position = startPos + Vector2.left * newPos;
+        if (activated)
+        {
+            mySprite.sprite = laserSprite;
+            newPos += 0.75f;
+            transform.position = startPos + Vector2.left * newPos;
+        }
+        else
+            mySprite.sprite = null;
+    }
+
+    public void Activate()
+    {
+        activated = true;
     }
 }
