a6c88687ee7084a5888dc11b343160d5c314caf9
Jojomonster0706 <bouboulover17@gmail.com>
2020-03-12 01:35:24 +0100

    ds

---
+++ Assets/Scripts/EnnemyComportment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyComportment : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+    public string ennemyTargetTag = "Player";
+    void Start()
+    {
+        Invoke("UpdateTarget", 0f);
+    }
+
+    
+    void Update()
+    {
+        GameObject[] ennemies = GameObject.FindGameObjectsWithTag(ennemyTargetTag);
+    }
+}
---
+++ Assets/Scripts/LaserShotAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShotAnimations : MonoBehaviour
+{
+
+    public float newPos;
+    private float scrollSpeed = -30f;
+    private Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+       
+            //19.625 et 25.207
+            newPos += 0.75f;
+      
+        
+
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
