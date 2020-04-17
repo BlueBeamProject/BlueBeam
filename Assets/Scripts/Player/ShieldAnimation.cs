d96dc02bbb22583b636a4c2ebd5d623cd0fe04de
Mamadoudette <vicbut10@gmail.com>
2020-04-17 03:08:06 +0200

    yes

--- Assets/Scripts/Player/ShieldAnimation.cs
+++ Assets/Scripts/Player/ShieldAnimation.cs
@@ -7,24 +7,20 @@ public class ShieldAnimation : MonoBehaviour
     public Sprite[] frames;
     public GameObject shield;
     private SpriteRenderer _mySpriteRenderer;
-    public float moveSpeed;
-    private Vector3 _movement;
-    private Transform _transform;
     private static bool stp;
+    public GameObject player;
+    public float animspeed;
 
     void Start()
     {
-        /*_mySpriteRenderer = GetComponent<SpriteRenderer>();
-        _movement = new Vector3(moveSpeed, 0, 0);
-        _transform = GetComponent<Transform>();*/
+        _mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        /*_transform.position += _movement * Time.deltaTime * moveSpeed;*/
+        transform.position = player.transform.position;
         if (stp)
         {
-            Debug.Log("stp");
             shield.SetActive(true);
             StartCoroutine(shildAnimation());
         }
@@ -32,7 +28,6 @@ public class ShieldAnimation : MonoBehaviour
 
     public static void ShildAn()
     {
-        Debug.Log("ShildAn");
         stp = true;
     }
 
@@ -44,18 +39,18 @@ public class ShieldAnimation : MonoBehaviour
     IEnumerator shildAnimation()
     {
         _mySpriteRenderer.sprite = frames[0];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[1];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[2];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[3];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[4];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[5];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
         _mySpriteRenderer.sprite = frames[6];
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(animspeed);
     }
 }
