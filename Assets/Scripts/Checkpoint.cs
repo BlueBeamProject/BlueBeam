6ff7a8148275bfa7a9ee26ef1db3f4bf616fe974
SamyGris <samy.gris@epita.fr>
2020-04-20 00:10:54 +0200

    Checkpoint ne fonctionne toujours pas

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private Transform _transform;
+    public GameObject player;
+    private Transform _transformPlayer;
     public GameObject camera;
     private Transform _transformCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        _transform = GetComponent<Transform>();
+        _transformPlayer = player.GetComponent<Transform>();
         _transformCamera = camera.GetComponent<Transform>();
+
         if (SaveData.ReadValueInt("checkpoint1") == 1)
         {
-          _transform.position += new Vector3(200, 0, 0);
-          _transformCamera.position += new Vector3(200, 0, 0);
+        _transformPlayer.position += new Vector3(200, 0, 0);
+        _transformCamera.position += new Vector3(200, 0, 0);
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_transform.position.x == 200)
+        if (_transformPlayer.position.x > 200)
           SaveData.WriteValueInt("checkpoint1", 1);
     }
 }
