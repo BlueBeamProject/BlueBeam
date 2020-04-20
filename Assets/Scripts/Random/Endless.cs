82b1778ffb1fe6dabf91a3aa2a556dc0cbdc22ae
Mamadoudette <vicbut10@gmail.com>
2020-04-20 04:17:01 +0200

    push un peu ca fait longtemps mais j'ai rien fait mdr oups

---
+++ Assets/Scripts/Random/Endless.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endless : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] routes;
+
+    private int pos;
+    private int times;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
