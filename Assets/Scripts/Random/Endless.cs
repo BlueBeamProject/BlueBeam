5d650c1c58fb582d15178cbb716f0f5258705732
Mamadoudette <vicbut10@gmail.com>
2020-04-20 04:56:00 +0200

    pourquoi ca glisse sur le putain de sol j'vais tout casser

--- Assets/Scripts/Random/Endless.cs
+++ Assets/Scripts/Random/Endless.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class Endless : MonoBehaviour
 {
     public GameObject player;
-    public GameObject[] routes;
+    public GameObject routes;
+    public int largeur;
 
     private int pos;
-    private int times;
+    private int lastplat;
+    private float hauteur;
 
     void Start()
     {
-
+        lastplat = 0;
+        pos = 0;
+        SpawnPlat(new Vector3(0, -5.5f));
+        SpawnPlat(new Vector3(largeur, -5.5f));
+        SpawnPlat(new Vector3(largeur+largeur, -5.5f));
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.x > pos)
+        {
+            if (lastplat == 0)
+            {
+                hauteur = -5.5f;
+                lastplat = Random.Range(0, 2);
+            }
+            else if (lastplat == 1)
+            {
+                hauteur = -3.8f;
+                lastplat = Random.Range(0, 3);
+            }
+            else
+            {
+                hauteur = -2.1f;
+                lastplat = Random.Range(0, 4);
+            }
+            pos += largeur;
+            SpawnPlat(new Vector3(12+pos, hauteur));
+        }
+    }
 
+    private void SpawnPlat(Vector3 spawnPosition)
+    {
+        Instantiate(routes, spawnPosition, Quaternion.identity);
     }
 }
