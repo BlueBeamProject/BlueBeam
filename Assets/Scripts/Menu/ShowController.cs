13f597e345ae7d39d1c7d7bc988b7f7908bf046f
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-04-21 15:14:05 +0200

    Affichage des touches de controlle

--- Assets/Scripts/Menu/ShowController.cs
+++ Assets/Scripts/Menu/ShowController.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ShowControlle : MonoBehaviour
+public class ShowController : MonoBehaviour
 {
+    
+    private static GameObject JumpP1;
+    private static GameObject SlideP1;
+    private static GameObject AttackP1;
+    private static GameObject DashP1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Refresh();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void Refresh()
+    {
+        JumpP1 = GameObject.FindGameObjectWithTag("JumpP1Button");
+        JumpP1.GetComponent<Text>().text = SaveData.ReadValueString("JumpP1");
+        
+        SlideP1 = GameObject.FindGameObjectWithTag("SlideP1Button");
+        SlideP1.GetComponent<Text>().text = SaveData.ReadValueString("SlideP1");
+        
+        AttackP1 = GameObject.FindGameObjectWithTag("AttackP1Button");
+        AttackP1.GetComponent<Text>().text = SaveData.ReadValueString("AttackP1");
+        
+        DashP1 = GameObject.FindGameObjectWithTag("DashP1Button");
+        DashP1.GetComponent<Text>().text = SaveData.ReadValueString("DashP1");
     }
 }
