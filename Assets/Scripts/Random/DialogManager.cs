f19aaafb4c5490f341546c6c7fcb379a51621f83
Jojomonster0706 <55802458+Jojomonster0706@users.noreply.github.com>
2020-04-22 03:37:06 +0100

    Dialogue (work in progress)

--- Assets/Scripts/Random/DialogManager.cs
+++ Assets/Scripts/Random/DialogManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    private GameObject pauseUI;
+    private PauseMenu _pauseMenu;
+
     void Start()
     {
-        
+        _pauseMenu = pauseUI.GetComponent<PauseMenu>();
+        PauseMenu.gameIsPaused = false;
+        StartCoroutine(Type());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PauseMenu.gameIsPaused = false;
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 }
