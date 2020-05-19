dd1097c8d6f419bc0c80f6480bc3a050a3647d81
Stanley-schillaci <stanley.schillaci@epita.fr>
2020-05-19 23:34:02 +0200

    First push menu V2 (pas fini)

---
+++ Assets/Scripts/Menu/ImageHover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ImageHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject text;
+    private Image im;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("start");
+
+        im = text.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+        im.rectTransform.sizeDelta = new Vector2(150,150);
+        
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        im.rectTransform.sizeDelta = new Vector2(100,100);
+    }
+}
---
+++ Assets/Scripts/Menu/TextHover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject text;
+    private Text tm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("start");
+
+        tm = text.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print(text.GetComponent<Text>().fontSize);
+        
+        tm.fontSize = 50; //Or however you do your color
+       // tm.color = Color.red;
+        tm.fontStyle = FontStyle.Bold;
+        print("i'm here");
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tm.fontSize = 35;
+        tm.color = Color.white;
+        tm.fontStyle = FontStyle.Normal;
+    }
+}
