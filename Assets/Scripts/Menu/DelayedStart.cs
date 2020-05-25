1510c5dbd09afd405b751dcef1773dcb2b7ee7ce
Jojomonster0706 <55802458+Jojomonster0706@users.noreply.github.com>
2020-05-25 11:30:03 +0100

    bug pause menu

--- Assets/Scripts/Menu/DelayedStart.cs
+++ Assets/Scripts/Menu/DelayedStart.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DelayedStart : MonoBehaviour
 {
     public GameObject countDown;
-    public  AudioSource someSound;
+    public AudioSource someSound;
     // Start is called before the first frame update
     void Start()
     {
